02f6bea62fbaab0c32433073b167f7a0eb7f21b6
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2024-04-27 11:25:36 -0700

    Successfully ran QMaster32 wrapper demo

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,85 @@
 const std = @import("std");
+const SDL = @import("sdl2");
+const target_os = @import("builtin").os;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    try SDL.init(.{
+        .video = true,
+        .events = true,
+        .audio = true,
+    });
+    defer SDL.quit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var window = try SDL.createWindow(
+        "SDL.zig Basic Demo",
+        .{ .centered = {} },
+        .{ .centered = {} },
+        640,
+        480,
+        .{ .vis = .shown },
+    );
+    defer window.destroy();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var renderer = try SDL.createRenderer(window, null, .{ .accelerated = true });
+    defer renderer.destroy();
 
-    try bw.flush(); // don't forget to flush!
+    mainLoop: while (true) {
+        while (SDL.pollEvent()) |ev| {
+            switch (ev) {
+                .quit => {
+                    break :mainLoop;
+                },
+                .key_down => |key| {
+                    switch (key.scancode) {
+                        .escape => break :mainLoop,
+                        else => std.log.info("key pressed: {}\n", .{key.scancode}),
+                    }
+                },
+
+                else => {},
+            }
+        }
+
+        try renderer.setColorRGB(0, 0, 0);
+        try renderer.clear();
+
+        try renderer.setColor(SDL.Color.parse("#F7A41D") catch unreachable);
+        try renderer.drawRect(SDL.Rectangle{
+            .x = 270,
+            .y = 215,
+            .width = 100,
+            .height = 50,
+        });
+
+        if (target_os.tag != .linux) {
+            // Ubuntu CI doesn't have this function available yet
+            try renderer.drawGeometry(
+                null,
+                &[_]SDL.Vertex{
+                    .{
+                        .position = .{ .x = 400, .y = 150 },
+                        .color = SDL.Color.rgb(255, 0, 0),
+                    },
+                    .{
+                        .position = .{ .x = 350, .y = 200 },
+                        .color = SDL.Color.rgb(0, 0, 255),
+                    },
+                    .{
+                        .position = .{ .x = 450, .y = 200 },
+                        .color = SDL.Color.rgb(0, 255, 0),
+                    },
+                },
+                null,
+            );
+        }
+
+        renderer.present();
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
