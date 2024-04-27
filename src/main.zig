534d8ceca368eb78607bd9c0888d351ee01c81f6
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2024-04-27 15:21:40 -0700

    best_display and scale_display implemented

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,13 @@ const FPS = 60;
 
 pub fn main() !void {
     const fullscreen = false;
-    var zoom: f64 = 0.0;
+    var zoom: f32 = 0.0;
 
     const risc_rect = SDL.SDL_Rect{
         .x = 0,
         .y = 0,
-        .w = 640,
-        .h = 480,
+        .w = 1024,
+        .h = 768,
     };
 
     if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0)
@@ -24,32 +24,29 @@ pub fn main() !void {
     _ = SDL.SDL_SetHint(SDL.SDL_HINT_RENDER_SCALE_QUALITY, "best");
 
     var window_flags: u32 = SDL.SDL_WINDOW_HIDDEN;
-    var display: c_uint = 0;
+    var display: c_int = 0;
 
     if (fullscreen) {
         window_flags |= SDL.SDL_WINDOW_FULLSCREEN_DESKTOP;
-        display = 0;
+        display = best_display(risc_rect);
     }
     if (zoom == 0.0) {
-        // SDL_Rect bounds;
-        // if (SDL_GetDisplayBounds(display, &bounds) == 0 &&
-        //     bounds.h >= risc_rect.h * 2 && bounds.w >= risc_rect.w * 2) {
-        // zoom = 2;
-        // } else {
-        // zoom = 1;
-        // }
-        zoom = 1.0;
+        var bounds: SDL.SDL_Rect = undefined;
+        if (SDL.SDL_GetDisplayBounds(display, &bounds) == 0 and
+            bounds.h >= risc_rect.h * 2 and bounds.w >= risc_rect.w * 2)
+        {
+            zoom = 2.0;
+        } else {
+            zoom = 1.0;
+        }
     }
+
     const window = SDL.SDL_CreateWindow(
         "Project Oberon",
-        SDL.SDL_WINDOWPOS_CENTERED,
-        SDL.SDL_WINDOWPOS_CENTERED,
-        // @as(c_int, SDL.SDL_WINDOWPOS_UNDEFINED_DISPLAY(display)),
-        // @as(c_int, SDL.SDL_WINDOWPOS_UNDEFINED_DISPLAY(display)),
-        //@as(c_int, 640 * zoom),
-        //@as(c_int, 480 * zoom),
-        640,
-        480,
+        SDL.SDL_WINDOWPOS_UNDEFINED_DISPLAY(0), // FixMe: should be 'display'
+        SDL.SDL_WINDOWPOS_UNDEFINED_DISPLAY(0), // FixMe: should be 'display'
+        @intFromFloat(@as(f32, risc_rect.w) * zoom),
+        @intFromFloat(@as(f32, risc_rect.h) * zoom),
         window_flags,
     ) orelse sdlPanic();
     defer _ = SDL.SDL_DestroyWindow(window);
@@ -59,7 +56,9 @@ pub fn main() !void {
 
     const texture = SDL.SDL_CreateTexture(renderer, SDL.SDL_PIXELFORMAT_ABGR8888, SDL.SDL_TEXTUREACCESS_STREAMING, risc_rect.w, risc_rect.h) orelse sdlPanic();
 
-    const display_rect = SDL.SDL_Rect{ .x = 0, .y = 0, .w = 640, .h = 480 };
+    var display_rect: SDL.SDL_Rect = undefined;
+    _ = scale_display(window, risc_rect, &display_rect);
+    std.debug.print("x: {} y: {} w: {} h: {}", .{ display_rect.x, display_rect.y, display_rect.w, display_rect.h });
 
     SDL.SDL_ShowWindow(window);
     _ = SDL.SDL_RenderClear(renderer);
@@ -83,8 +82,8 @@ pub fn main() !void {
         SDL.SDL_RenderPresent(renderer);
 
         // const frame_end: u32 = SDL.SDL_GetTicks();
-        // const delay: i32 = frame_start + 1000 / FPS - frame_end;
-        const delay = 16;
+        // const delay: u32 = frame_start + 16 - frame_end;
+        const delay: u32 = 17;
 
         if (delay > 0) {
             SDL.SDL_Delay(delay);
@@ -95,6 +94,50 @@ pub fn main() !void {
 fn sdlPanic() noreturn {
     const str = @as(?[*:0]const u8, SDL.SDL_GetError()) orelse "unknown error";
     @panic(std.mem.sliceTo(str, 0));
+}
+
+fn best_display(rect: SDL.SDL_Rect) u8 {
+    var best: u8 = 0;
+    const display_cnt: c_int = SDL.SDL_GetNumVideoDisplays();
+    if (display_cnt < 0) sdlPanic();
+    var i: c_int = 0;
+    while (i < display_cnt) : (i += 1) {
+        var bounds: SDL.SDL_Rect = undefined;
+        if (SDL.SDL_GetDisplayBounds(@as(c_int, @truncate(i)), &bounds) == 0 and
+            bounds.h == rect.h and bounds.w >= rect.w)
+        {
+            best = @intCast(i);
+            if (bounds.w == rect.w) {
+                break;
+            }
+        }
+    }
+    return best;
+}
+
+fn scale_display(window: *SDL.SDL_Window, risc_rect: SDL.SDL_Rect, display_rect: *SDL.SDL_Rect) f32 {
+    var win_w: c_int = undefined;
+    var win_h: c_int = undefined;
+    SDL.SDL_GetWindowSize(window, &win_w, &win_h);
+    const oberon_aspect: f32 = @as(f32, @floatFromInt(risc_rect.w)) / @as(f32, @floatFromInt(risc_rect.h));
+    const window_aspect: f32 = @as(f32, @floatFromInt(win_w)) / @as(f32, @floatFromInt(win_h));
+
+    var scale: f32 = undefined;
+    if (oberon_aspect > window_aspect) {
+        scale = @as(f32, @floatFromInt(win_w)) / @as(f32, @floatFromInt(risc_rect.w));
+    } else {
+        scale = @as(f32, @floatFromInt(win_h)) / @as(f32, @floatFromInt(risc_rect.h));
+    }
+
+    const w = @ceil(@as(f32, @floatFromInt(risc_rect.w)) * scale);
+    const h = @ceil(@as(f32, @floatFromInt(risc_rect.h)) * scale);
+
+    display_rect.*.w = @intFromFloat(w);
+    display_rect.*.h = @intFromFloat(h);
+    display_rect.*.x = @intFromFloat((@as(f32, @floatFromInt(win_w)) - w) / 2.0);
+    display_rect.*.y = @intFromFloat((@as(f32, @floatFromInt(win_h)) - h) / 2.0);
+
+    return scale;
 }
 
 // test "simple test" {
